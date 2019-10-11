381e2124b4b1506410b320bddc65d192b2b5e53b
Antoine <antbury28@gmail.com>
2019-10-11 15:53:07 +0200

    Fix textMesh pro

--- Grille/Assets/GridManager.cs
+++ Grille/Assets/GridManager.cs
@@ -8,55 +8,50 @@ using TMPro;
 public class GridManager : MonoBehaviour
 {
     private GameObject tileReference;
-    private int ligne = 2, colonne = 2;
+    private int ligne, colonne;
     private float espacement = 1.1f;
     public Sprite sprite;
-    private Grille<int> grille2;
+    private Grille<int> grille;
 
-    void Start()
+    public GridManager(Grille<int> grille)
     {
         tileReference = GameObject.Find("TilePrefab");
-        grille2 = new Grille<int>(ligne, colonne);
-        grille2.setVal(10);
-        GenerateGrid();
-    }
-    
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            UpdateGrid();
-        }
+        this.grille = grille;
+        this.colonne = grille.getCols();
+        this.ligne = grille.getRows();
     }
 
-    private void GenerateGrid()
+    public void GenerateGrid(float posX, float posY, Transform parent)
     {
-        Transform parent = GameObject.Find("GridManager").transform;
         for (int i = 0; i < this.ligne; i++)
         {
             for (int j = 0; j < this.colonne; j++)
             {
-                Debug.Log(0.1f * (int)(this.colonne / 2));
-                //Vector2 pos = new Vector2(j * espacement - this.colonne/2 - (this.colonne - 1/2)*espacement, i * espacement - this.ligne / 2);
-                Vector2 pos = new Vector2((j*espacement - (this.colonne - 1)*espacement/2), (i * espacement - (this.ligne - 1) * espacement / 2));
+                Vector2 pos = new Vector2(posX + (j * espacement - (this.colonne - 1) * espacement / 2), posY + (i * -espacement - (this.ligne - 1) * -espacement / 2));
                 GameObject tile = Instantiate(tileReference, pos, tileReference.transform.rotation, parent);
                 tile.name = "Case" + i + "_" + j;
-                tile.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = grille2.getVal(i, j).ToString();
-                //Debug.Log("grille.tab[" + i + "][" + j + "] : " + grille2.getVal(i, j));
+                afficher(i, j, tile);
             }
         }
-        //Debug.Log("Grid created");
     }
 
-    private void UpdateGrid()
+    public void UpdateGrid()
     {
         for (int i = 0; i < this.ligne; i++)
         {
             for (int j = 0; j < this.colonne; j++)
             {
                 GameObject tile = GameObject.Find("Case" + i + "_" + j);
-                tile.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = grille2.getVal(i, j).ToString();
+                afficher(i, j, tile);
             }
         }
+    }
+
+    private void afficher(int i, int j, GameObject tile)
+    {
+        if(this.grille.getVal(i, j) != -1) 
+            tile.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = this.grille.getVal(i, j).ToString();
+        else
+            tile.GetComponent<SpriteRenderer>().color = Color.black;
     }
 }

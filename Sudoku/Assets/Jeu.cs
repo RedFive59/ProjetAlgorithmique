a23bc7e45fea5af5b8b710eb6c9ddeb1d51d0733
Charles <ctacquet@outlook.fr>
2019-10-11 20:44:24 +0200

    Initialisation du projet Sudoku

---
+++ Sudoku/Assets/Jeu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jeu : MonoBehaviour
+{
+    void Start()
+    {
+        Grille<int> grille = new Grille<int>(9, 9);
+        grille.setVal(0);
+        GridManager g = new GridManager(grille);
+        Transform parent = GameObject.Find("GridManager").transform;
+        g.GenerateGrid(0f, 0f, parent);
+    }
+}

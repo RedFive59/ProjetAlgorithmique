25203389b24deb38cd8717583d00098803c66df9
Antoine <minighost@free.fr>
2019-10-11 13:24:22 +0200

    Création des classes Carte, Joueur, Main et Paquet

---
+++ Poker/Carte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Carte
+{
+
+	enum COULEUR { COEUR, CARREAU, TREFLE, PIQUE };
+	enum VALEUR { DEUX=2, TROIS, QUATRE, CINQ, SIX, SEPT, HUIT, NEUF, DIX, VALET, DAME, ROI, AS};
+
+    public Carte() { }
+
+    public COULEUR couleur { get; set; }
+    public VALEUR valeur { get; set; }
+
+}
---
+++ Poker/Joueur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Joueur
+{
+	// +-----------+
+	// | Attributs |
+	// +-----------+
+
+	private String nom;
+	private int num;
+	private static int id;
+	private float gains;
+	private Main main;
+
+
+
+	public Joueur() { }
+
+
+}

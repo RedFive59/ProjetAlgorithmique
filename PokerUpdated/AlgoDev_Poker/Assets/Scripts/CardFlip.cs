9f77abacd57fffe292533ea9c069f500ca5e2479
Antoine <minighost@free.fr>
2019-10-16 16:22:03 +0200

    Version Unity du Poker 1.0
    - Création et affichage des cartes
    - Génération et affichage d'une "pioche" aléatoire

---
+++ PokerUpdated/AlgoDev_Poker/Assets/Scripts/CardFlip.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlip : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    CardModel model;
+
+    public AnimationCurve scaleCurve;
+    public float duree = 0.5f;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        model = GetComponent<CardModel>();
+    }
+
+    public void flipCard(Sprite startImage, Sprite endImage, int pointeurCarte)
+    {
+        StopCoroutine(Flip(startImage, endImage, pointeurCarte));
+        StartCoroutine(Flip(startImage, endImage, pointeurCarte));
+    }
+
+    IEnumerator Flip(Sprite startImage, Sprite endImage, int pointeurCarte)
+    {
+        spriteRenderer.sprite = startImage;
+        float time = 0f;
+        while (time <= 1f)
+        {
+            float scale = scaleCurve.Evaluate(time);
+            time += Time.deltaTime / duree;
+
+            Vector3 localScale = transform.localScale;
+            localScale.x = scale;
+            transform.localScale = localScale;
+
+            if (time >= 0.5f)
+            {
+                spriteRenderer.sprite = endImage;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        if (pointeurCarte == -1)
+        {
+            model.changerFace(false);
+        }
+        else
+        {
+            model.pointeurCartes = pointeurCarte;
+            model.changerFace(true);
+        }
+    }
+}
---
+++ PokerUpdated/AlgoDev_Poker/Assets/Scripts/DeckView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Pioche))]
+
+public class DeckView : MonoBehaviour
+{
+    Pioche pioche;
+
+    public Vector3 start;
+    public float cardOffset; //distance séparant chaque carte à l'affichage
+    public GameObject prefabCarte;
+
+    void Start()
+    {    
+        pioche = GetComponent<Pioche>();
+        showCards();
+    }
+
+    void showCards()
+    {
+        int cardCount = 0;
+
+        foreach(int i in pioche.GetCartes())
+        {
+            float co = cardOffset * cardCount;
+
+            GameObject cardCopy = (GameObject)Instantiate(prefabCarte);
+            Vector3 temp = start + new Vector3(co, 0f);
+            cardCopy.transform.position = temp;
+
+            CardModel cardModel = cardCopy.GetComponent<CardModel>();
+            cardModel.pointeurCartes = i;
+            cardModel.changerFace(true);
+
+            cardCount++;
+        }
+    }
+}

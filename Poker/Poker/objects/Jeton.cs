6a6ec4ce6334404b12a5ce86886ae3d3e5fa447e
Nicolas MARTIN <nyflola@gmail.com>
2019-10-16 20:25:11 +0200

    Joueur, Main, Jeton, Mise
    Création et implémentation des classes citées

---
+++ Poker/Poker/objects/Jeton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Jeton
+{
+    public enum Valeur { BLANC = 1, ROUGE = 5, VERT = 10, BLEU = 25, NOIR = 50 };
+    //Attributs
+    private Valeur valeur;
+    //Constructeurs
+	public Jeton(Valeur valeur)
+	{
+        this.valeur = valeur;
+    }
+    //Méthodes
+    public Valeur getValeur()
+    {
+        return this.valeur;
+    }
+    public bool memeValeur(Jeton j)
+    {
+        if ((int)this.valeur == (int)j.getValeur()) return true;
+        return false;
+    }
+}

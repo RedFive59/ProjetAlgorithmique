8ff5aa75281593d75210c94d9eeebb06e7ceaa5f
Telessia <44902908+Telessia@users.noreply.github.com>
2019-10-18 08:44:55 +0200

    Ajout script selection des cases/placement caméra
    +Selection.cs
    +CameraManager.cs

---
+++ BatailleNavale/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    Camera CameraM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraM = Camera.main;
+        CameraM.enabled = true;
+        CameraM.orthographic = true;
+        CameraM.orthographicSize = 5.8f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BatailleNavale/Selection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour
+{
+    private Color startColor;
+    private SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        startColor = rend.color;
+
+    }
+    void OnMouseEnter()
+    {
+        rend.color = Color.red;
+    }
+    void OnMouseExit()
+    {
+        rend.color = startColor;
+    }
+}

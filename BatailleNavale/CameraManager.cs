20874e1acc84ed972d1aa582101c16be1071d472
Telessia <44902908+Telessia@users.noreply.github.com>
2019-10-20 13:09:17 +0200

    Scripts GridManager/CameraManager commentés

--- BatailleNavale/CameraManager.cs
+++ BatailleNavale/CameraManager.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    Camera CameraM;
+    Camera CameraM; //Declare une variable de type camera et récupère la camera à laquelle le script est attaché
 
     // Start is called before the first frame update
     void Start()
     {
-        CameraM = Camera.main;
-        CameraM.enabled = true;
-        CameraM.orthographic = true;
-        CameraM.orthographicSize = 5.8f;
-
+        CameraM = Camera.main; //la camere est déclarée en tant que caméra principale de la scene
+        CameraM.enabled = true; //la camera est active
+        CameraM.orthographic = true; //camera est en mode orthographique
+        CameraM.orthographicSize = 5.8f; //déclare la taille (diagonale) du rectangle que couvre la camera
+        //la position initiale n'est pas changée la caméra est centrée sur l'origine de la scène
     }
 
     // Update is called once per frame
--- BatailleNavale/GridManager.cs
+++ BatailleNavale/GridManager.cs
@@ -6,10 +6,10 @@ public class GridManager : MonoBehaviour
 {
     private int rows = 11;
     private int cols = 11;
-    private Grille<int> col0;
-    private Grille<char> row0;
-    private Grille<int> grille;
-    public Sprite WaterDiffuseMini;
+    private Grille<int> col0; //vecteur qui contiendra la colonne des chiffres (juste visuel) 
+    private Grille<char> row0; //vecteur qui contiendra la ligne des lettres (juste visuel)
+    private Grille<int> grille; //matrice qui contiendra les valeurs de l'eau/bateau, libre/raté/touché
+    public Sprite WaterDiffuseMini; //var texture des cases d'eau
 
     // Start is called before the first frame update
     void Start()
@@ -22,35 +22,35 @@ public class GridManager : MonoBehaviour
         for (int i = 0; i < rows; i++)
         {
             col0.ajoutVect(i, i);
-            row0.ajoutVect(i, System.Convert.ToChar(65 + i));
+            row0.ajoutVect(i, System.Convert.ToChar(65 + i)); //convertit un entier en char
             }
         grille.setVal(0);
         ShowGrid(col0, row0, grille);
     }
 
-    void ShowGrid(Grille<int> col00, Grille<char> row00, Grille<int> grille00)
+    void ShowGrid(Grille<int> col00, Grille<char> row00, Grille<int> grille00)//affiche les différentes matrices/vecteurs
     {
         int i, j = 0;
         for (i = 0; i < rows; i++)
         {
-            CreateTileNumber(i, 0, col00.getVal(i));
-            CreateTileChar(0, i, row00.getVal(i));
+            CreateTileNumber(i, 0, col00.getVal(i));//creer un sprite avec un textechiffre
+            CreateTileChar(0, i, row00.getVal(i));//creer un sprite avec un texte lettre
         }
         for (i = 0; i < rows - 1; i++)
         {
             for (j = 0; j < cols - 1; j++)
-                CreateTileWater(i+1, j+1, grille.getVal(i, j));
+                CreateTileWater(i+1, j+1, grille.getVal(i, j));//creer les cases d'eau
         }
     }
 
-        void CreateTileNumber(int i, int j, int v)
+        void CreateTileNumber(int i, int j, int v)//voir CreateTileWater
         {
             GameObject t = new GameObject("X:" + i + "Y:" + j);
             t.transform.position = new Vector3(i - rows / 2, j - cols / 2);
             t.AddComponent<SpriteRenderer>().sprite = WaterDiffuseMini;
          }
 
-    void CreateTileChar(int i, int j, char v)
+    void CreateTileChar(int i, int j, char v)//voir CreateTileWater
     {
         GameObject t = new GameObject("X:" + i + "Y:" + j);
         t.transform.position = new Vector3(i - rows / 2, j - cols / 2);
@@ -59,12 +59,12 @@ public class GridManager : MonoBehaviour
 
     void CreateTileWater(int i, int j, int v)
         {
-            GameObject t = new GameObject("X:" + i + "Y:" + j);
-            t.transform.position = new Vector3(i - rows / 2, j - cols / 2);
-            t.AddComponent<SpriteRenderer>().sprite = WaterDiffuseMini;
-            BoxCollider2D b = new BoxCollider2D();
-            t.AddComponent<BoxCollider2D>().autoTiling = b;
-            t.AddComponent<Selection>();
+            GameObject t = new GameObject("X:" + i + "Y:" + j);//creer un gameObject sprite avec un nom donné
+            t.transform.position = new Vector3(i - rows / 2, j - cols / 2);//place le gameObject dans la scene
+            t.AddComponent<SpriteRenderer>().sprite = WaterDiffuseMini;//creer et attache un rendu au sprite et lui attribut la texture de l'eau
+            BoxCollider2D b = new BoxCollider2D();//creer un collider rectangulaire 2D
+            t.AddComponent<BoxCollider2D>().autoTiling = b;//attache le collider précédent au sprite en le resizant automatiquement au sprite auquel il est attaché
+            t.AddComponent<Selection>();//attache au sprite le script selection
         }
 
         // Update is called once per frame

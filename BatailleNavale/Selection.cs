a281caf9c5ba759fa9551d3651bc79aa9cdf72b1
Telessia <44902908+Telessia@users.noreply.github.com>
2019-10-20 13:08:01 +0200

    Script selection commenté

--- BatailleNavale/Selection.cs
+++ BatailleNavale/Selection.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Selection : MonoBehaviour
 {
-    private Color startColor;
-    private SpriteRenderer rend;
+    private Color startColor; //var qui contiendra la couleur originale du sprite ciblé
+    private SpriteRenderer rend; //var qui contiendra le rendu (spriteRenderer) du sprite ciblé
 
     void Start()
     {
-        rend = GetComponent<SpriteRenderer>();
-        startColor = rend.color;
+        rend = GetComponent<SpriteRenderer>(); //extrait et stock le rendu du sprite
+        startColor = rend.color;   //récupère la couleur original depuis le rendu
 
     }
     void OnMouseEnter()
     {
-        rend.color = Color.red;
+        rend.color = Color.red; //la souris est sur le sprite sa nouvelle couleur devient rouge
     }
     void OnMouseExit()
     {
-        rend.color = startColor;
+        rend.color = startColor; //la souris sort du sprite il reprend sa couleur d'origine
     }
 }

962b35d6c9a0bec47faf30e80cc6c8e3cd70e5f6
KhiGamma <antbury28@gmail.com>
2019-10-24 22:03:10 +0200

    Interface Bingo + Menu option Bingo + Menu de selection des jeux

---
+++ Bingo/Assets/Bingo/SettingWait.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SettingWait : MonoBehaviour
+{
+    private int nb;
+
+    public void getTempsAttente(float value)
+    {
+        int nouv = (int)(value);
+        this.nb = nouv;
+        setTempsAttente();
+    }
+
+    public void setTempsAttente()
+    {
+        GameObject value = GameObject.Find("VariableAttente");
+        GameObject value2 = GameObject.Find("VariableAttenteDisp");
+        value.transform.GetComponent<TextMeshProUGUI>().text = this.nb.ToString();
+        value2.transform.GetComponent<TextMeshProUGUI>().text = this.nb.ToString();
+    }
+}

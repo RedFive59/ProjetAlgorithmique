7b4cc62ae79a6ed57a03f9a1eabaa055cd246cb7
Xavier <telessiasilent@gmail.com>
2019-10-25 23:31:26 +0200

    Update magasin + ajout script selection 2
    Préparation du double panel dans le script magasin avec animation, ajout d'un script "selection2" non fonctionnel pour le déplacement des panel, sera surement remplacé/supprimé sur le court terme (meilleure solution étudiée en ce moment).

---
+++ Jeu/Assets/BatailleNavale/Scripts/Selection2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection2 : MonoBehaviour
+{
+    RectTransform rects2;
+    Vector3 vs2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rects2 = GetComponent<RectTransform>();
+        vs2 = rects2.localPosition;
+    }
+
+    void OnMouseEnter()
+    {
+        rects2.localPosition = new Vector3(271f, 0, 0);
+
+    }
+
+    void OnMouseExit()
+    {
+        rects2.localPosition = vs2;
+    }
+}

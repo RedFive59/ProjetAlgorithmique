165a39ad21ad59ae9b142afe5a91a25ee3ca09e5
Xavier <telessiasilent@gmail.com>
2019-10-26 22:23:12 +0200

    Update ouverture/fermeture Magasin + fix texte + opti script selection
    -Suppression de variables inutiles dans le script de selection de l'eau.
    +Ajout d'un deuxième panel servant de bouton dans le magasin pour plus de clarté.
    +Fix du texte "Magasin" affiché à la verticale maintenant au lieu de l'horizontale.

--- Jeu/Assets/BatailleNavale/Scripts/Magasin.cs
+++ Jeu/Assets/BatailleNavale/Scripts/Magasin.cs
@@ -7,7 +7,6 @@ public class Magasin : MonoBehaviour
 {
     Canvas myCanvas2;
     GameObject cvs2;
-    Color sgrey;//la couleur qui sera utilisée pour le panel
     RectTransform rect2;
     RectTransform rect3;
     private int Magasinpos;
@@ -21,6 +20,7 @@ public class Magasin : MonoBehaviour
         cvs2.AddComponent<Canvas>();
         cvs2.AddComponent<CanvasScaler>().dynamicPixelsPerUnit=50;
         cvs2.AddComponent<GraphicRaycaster>();
+        Color sgrey;//la couleur qui sera utilisée pour le panel
 
         myCanvas2 = cvs2.GetComponent<Canvas>();
         //myCanvas2.renderMode = RenderMode.ScreenSpaceCamera; //canvas en mode resize sur la camera
@@ -33,36 +33,51 @@ public class Magasin : MonoBehaviour
         rect3.sizeDelta = new Vector2(11f,11f);
 
         GameObject panel = new GameObject("Panel");
+        panel.transform.SetParent(myCanvas2.transform, false);
         panel.AddComponent<CanvasRenderer>();
         Image I = panel.AddComponent<Image>();//récupération du pseudo rendu du panel
-        panel.transform.SetParent(myCanvas2.transform, false);
         panel.layer = 9;
 
         cvs2.transform.SetParent(this.transform, false);
 
         rect2 = panel.GetComponent<RectTransform>();//recup le recttransform du panel
-        rect2.sizeDelta = new Vector2(5.3f, 11.6f);//resize le panel
-        rect2.localPosition = new Vector3(6.1f, 0, 0);
-        sgrey.r = 140;//set la valeur du rvb rouge
-        sgrey.b = 140;//set la valeur du rvb bleu
-        sgrey.g = 140;//set la valeur du rvb vert
+        rect2.sizeDelta = new Vector2(6.127f, 11.6f);//resize le panel
+        rect2.localPosition = new Vector3(5.686f, 0, 0);
+
+        sgrey.r = 180;//set la valeur du rvb rouge
+        sgrey.b = 180;//set la valeur du rvb bleu
+        sgrey.g = 180;//set la valeur du rvb vert
         sgrey.a = 0.5f;//set la transparence de la couleur de min 0 à max 1
         I.color = sgrey;
 
         panel.AddComponent<VerticalLayoutGroup>();
-        panel.AddComponent<Button>();
-        panel.GetComponent<Button>().onClick.AddListener(MoveMagasin);
+
+        GameObject panel2 = new GameObject("PanelButton");
+        panel2.transform.SetParent(myCanvas2.transform, false);
+        panel2.AddComponent<CanvasRenderer>();
+        panel2.AddComponent<RectTransform>();
+        panel2.GetComponent<RectTransform>().sizeDelta = new Vector2(1f, 11.6f);
+        panel2.GetComponent<RectTransform>().localPosition = new Vector3(3.122499f, 0, 0);
+        panel2.AddComponent<Button>().onClick.AddListener(MoveMagasin);
+        Image I2 = panel2.AddComponent<Image>();//récupération du pseudo rendu du panel
+        sgrey.r = 10;
+        sgrey.b = 10;
+        sgrey.g = 10;
+        sgrey.a = 0.5f;
+        I2.color = sgrey;
 
         void MoveMagasin()
         {
             if (Magasinpos == 0)
             {
-                panel.transform.localPosition = new Vector3(6.1f, 0, 0);
+                panel.transform.localPosition = new Vector3(5.686f, 0, 0);
+                panel2.transform.localPosition = new Vector3(3.122499f, 0, 0);
                 Magasinpos = 1;
             }
             else { if (Magasinpos == 1)
                 {
                     panel.transform.localPosition = new Vector3(10.43f, 0, 0);
+                    panel2.transform.localPosition = new Vector3(7.8665f, 0, 0);
                     Magasinpos = 0;
                 }
          }
@@ -72,7 +87,7 @@ public class Magasin : MonoBehaviour
         Text text;
         RectTransform rectTransform;
         myText = new GameObject("Magasin");
-        myText.transform.parent = panel.transform;
+        myText.transform.parent = panel2.transform;
         text = myText.AddComponent<Text>();
         text.color = Color.black;//couleur de texte noire
         text.font = (Font)Resources.GetBuiltinResource<Font>("Arial.ttf");//utilise la police Arial pour afficher le texte
@@ -86,7 +101,8 @@ public class Magasin : MonoBehaviour
         rectTransform.localPosition = new Vector3(0, 0, 0);
         rectTransform.sizeDelta = new Vector2(5, 2);
         rectTransform.localScale = new Vector3(0.5f, 0.5f, 1);
-        rectTransform.rotation = new Quaternion(0f,0f,0f,90f);
+        //rectTransform.rotation = new Quaternion(0f,0f,0f,90f);
+        rectTransform.Rotate(new Vector3(0, 0, 90f));
     }
     // Update is called once per frame
     void Update()
--- Jeu/Assets/BatailleNavale/Scripts/Selection.cs
+++ Jeu/Assets/BatailleNavale/Scripts/Selection.cs
@@ -9,16 +9,15 @@ public class Selection : MonoBehaviour
 
     void Start()
     {
-        rend = GetComponent<SpriteRenderer>(); //extrait et stock le rendu du sprite
-        startColor = rend.color;   //récupère la couleur original depuis le rendu
+        startColor = this.GetComponent<SpriteRenderer>().color;   //récupère la couleur original depuis le rendu du sprite auquel le script est attaché
 
     }
     void OnMouseEnter()
     {
-        rend.color = Color.red; //la souris est sur le sprite sa nouvelle couleur devient rouge
+        this.GetComponent<SpriteRenderer>().color = Color.red; //la souris est sur le sprite sa nouvelle couleur devient rouge
     }
     void OnMouseExit()
     {
-        rend.color = startColor; //la souris sort du sprite il reprend sa couleur d'origine
+        this.GetComponent<SpriteRenderer>().color = startColor; //la souris sort du sprite il reprend sa couleur d'origine
     }
 }

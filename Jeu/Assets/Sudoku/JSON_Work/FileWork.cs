cf10e590d6f0792adfc70686316525094e8564c6
Charles <ctacquet@outlook.fr>
2019-11-04 12:28:23 +0100

    JSONNode fonctionnel avec des tableaux de tableaux de int, Remove de la sauvegarde

--- Jeu/Assets/Sudoku/JSON_Work/FileWork.cs
+++ Jeu/Assets/Sudoku/JSON_Work/FileWork.cs
@@ -6,92 +6,45 @@ using SimpleJSON;
 
 public class FileWork : MonoBehaviour
 {
-    JSONNode node;
-    public class GameData
-    {
-        public int[,] tab = {
-            { 1, 2, 3 },
-            { 4, 5, 6 },
-            { 7, 8, 9 }
-        };
-        public int[,] tab2 = {
-            { 0, 0, 1 },
-            { 1, 0, 1 },
-            { 0, 0, 0 }
-        };
-        public int[,] getTab()
-        {
-            return tab;
-        }
-        public int[,] getTab2()
-        {
-            return tab2;
-        }
-    }
-
     void Start()
     {
-        GameData g = new GameData();
-        SaveGameData(g);
-        //LoadGameData();
+        LoadGameData();
     }
 
-    private GameData LoadGameData()
+    private void LoadGameData()
     {
-        GameData loadedData = null;
         string gameDataFileName = "Sudoku/JSON_Work/Saves/saveFile.json";
         string filePath = Path.Combine(Application.dataPath, gameDataFileName);
 
         if (File.Exists(filePath))
         {
             string dataAsJson = File.ReadAllText(filePath);
-            loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+            var loadedData = JSON.Parse(dataAsJson);
 
-            int rowLength = loadedData.tab.GetLength(0);
-            int colLength = loadedData.tab.GetLength(1);
-
-            string res = "" , res2 = "";
-            res += "  [\n";
-            res2 += "  [\n";
-            for (int i = 0; i < rowLength; i++)
+            string res = "tab = [\n", res2 = "tabTrou = [\n";
+            int tmp, tmp2;
+            for (int i = 0; i < 9; i++)
             {
-                res += "\t[";
-                res2 += "\t[";
-                for (int j = 0; j < colLength; j++)
+                res += "[";
+                res2 += "[";
+                for (int j = 0; j < 9; j++)
                 {
-                    if(j != colLength - 1)
+                    if (j % 3 == 0 && j != 0)
                     {
-                        res += loadedData.tab.GetValue(i, j) + ", ";
-                        res2 += loadedData.tab2.GetValue(i, j) + ", ";
-                    } else
-                    {
-                        res += loadedData.tab.GetValue(i, j) + "]";
-                        res2 += loadedData.tab2.GetValue(i, j) + "]";
+                        res += " ";
+                        res2 += " ";
                     }
+                    tmp = loadedData["tab"][i][j];
+                    tmp2 = loadedData["tabTrou"][i][j];
+                    res += tmp;
+                    res2 += tmp2;
                 }
-                res += "\n";
-                res2 += "\n";
+                res += "]\n";
+                res2 += "]\n";
             }
-            res += "  ]";
-            res2 += "  ]";
-
+            res += "]";
+            res2 += "]";
             Debug.Log("Chargement effectué :\n" + res + "\n" + res2);
-        }
-        else Debug.Log("Fichier introuvable");
-        return loadedData;
-    }
-
-    private void SaveGameData(GameData g)
-    {
-        string gameDataFileName = "Sudoku/JSON_Work/Saves/saveFile.json";
-        string filePath = Path.Combine(Application.dataPath, gameDataFileName);
-
-        if (File.Exists(filePath))
-        {
-            string jsonText = JsonUtility.ToJson(g, true);
-            Debug.Log(jsonText);
-            File.WriteAllText(filePath, jsonText);
-            Debug.Log("Sauvegarde effectué");
         }
         else Debug.Log("Fichier introuvable");
     }

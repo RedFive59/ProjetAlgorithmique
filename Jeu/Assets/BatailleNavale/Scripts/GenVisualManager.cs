2a5ed3819729fa1a5c28f637801484423ef0dc9b
Xavier <telessiasilent@gmail.com>
2019-11-06 22:21:40 +0100

    Centralisation scripts et GO
    +Visual Holder (GO) avec GenVisualManager (script) pour centraliser les scripts et les GOs sur un même GO principale.

---
+++ Jeu/Assets/BatailleNavale/Scripts/GenVisualManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenVisualManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenPlateau(1);
+    }
+
+    void GenPlateau(int x)
+    {
+        GameObject GridHolder = new GameObject("GridHolder");
+        GridHolder.AddComponent<GridManagerNavale>();
+        GridHolder.transform.SetParent(this.transform, false);
+        GameObject ShipHolder = new GameObject("ShipHolder");
+        ShipHolder.AddComponent<GenShips>();
+        ShipHolder.transform.SetParent(this.transform, false);
+        GameObject UIHolder = new GameObject("UIHolder");
+        UIHolder.AddComponent<Magasin>();
+        UIHolder.transform.SetParent(this.transform, false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

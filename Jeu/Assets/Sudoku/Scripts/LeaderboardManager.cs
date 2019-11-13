9b63e037cf1c9c1ececf9e9570ac5a7495001b9b
Charles <ctacquet@outlook.fr>
2019-11-13 19:25:31 +0100

    Tri du leaderboard fonctionnel

--- Jeu/Assets/Sudoku/Scripts/LeaderboardManager.cs
+++ Jeu/Assets/Sudoku/Scripts/LeaderboardManager.cs
@@ -11,46 +11,77 @@ public class LeaderboardManager : MonoBehaviour
 {
     private GameObject leaderboard; // Référence à l'objet Leaderboard sur la scène
     private GameObject boardRef; // Référence à la ligne type du leaderboard
+    private GameObject parent;
     private string filePath; // Chemin du fichier d'historique des parties
+    private string[,] history; // Grille de strings dans lesquelles les données du leaderboard sont placés
+    private int nombreColonnes;
 
     // Méthode appelé au lancement de la scène SudokuMenu
     void Start()
     {
+        nombreColonnes = 3; // Nombre de colonnes de votre leaderboard
         filePath = Path.Combine(Application.dataPath, "Saves/Sudoku.json"); // Définition du chemin du fichier
         leaderboard = GameObject.Find("Leaderboard");
         boardRef = GameObject.Find("BoardReference");
-        if (File.Exists(filePath)) updateLeaderboard();
+        parent = GameObject.Find("Content");
+        if (File.Exists(filePath))
+        {
+            var loadedData = JSON.Parse(File.ReadAllText(filePath)); // Répartition des données dans loadedData
+            history = new string[loadedData["history"].Count, nombreColonnes];
+            updateLeaderboard();
+        }
         else Debug.LogError("Chargement de " + filePath + " impossible");
     }
     
     // Méthode qui permet l'ajout dans le tableau des scores de toutes les parties précédement jouées
     private void updateLeaderboard()
     {
-        GameObject parent = GameObject.Find("Content");
-        if (parent == null || boardRef == null || leaderboard == null) // Vérification de présence des objets
+        if (boardRef)
         {
-            Debug.LogError("Problème objet");
-            return;
-        }
-        var loadedData = JSON.Parse(File.ReadAllText(filePath)); // Répartition des données dans loadedData
-        if(!loadedData["history"] && loadedData["history"].Count != 0)
+            if (parent == null || leaderboard == null) // Vérification de présence des objets
+            {
+                Debug.LogError("Problème objet");
+                return;
+            }
+            var loadedData = JSON.Parse(File.ReadAllText(filePath)); // Répartition des données dans loadedData
+            if (!loadedData["history"] && loadedData["history"].Count != 0)
+            {
+                for (int i = 0; i < loadedData["history"].Count; i++)
+                {
+                    history[i, 0] = loadedData["history"][i][0];
+                    history[i, 1] = loadedData["history"][i][1];
+                    history[i, 2] = loadedData["history"][i][2];
+                }
+                for (int i = 0; i < loadedData["history"].Count; i++)
+                {
+                    Vector3 pos = boardRef.transform.position + new Vector3(0, -30 * i);
+                    GameObject boardTab = Instantiate(boardRef, pos, boardRef.transform.rotation, parent.transform);
+                    boardTab.name = "board" + (i + 1);
+                    boardTab.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = " " + history[i, 0];
+                    boardTab.transform.GetChild(7).GetComponent<TextMeshProUGUI>().text = " " + history[i, 1];
+                    boardTab.transform.GetChild(8).GetComponent<TextMeshProUGUI>().text = " " + history[i, 2];
+                }
+                if (loadedData["history"].Count < 16) // Vérification qui permet la disparition de la barre de scroll si le tableau n'est pas assez grand
+                {
+                    GameObject.Find("Scroll View").GetComponent<ScrollRect>().enabled = false;
+                    GameObject.Find("Scrollbar Vertical").SetActive(false);
+                }
+            }
+            Destroy(boardRef);
+        } else
         {
-            for (int i = 0; i < loadedData["history"].Count; i++)
+            var loadedData = JSON.Parse(File.ReadAllText(filePath)); // Répartition des données dans loadedData
+            if (!loadedData["history"] && loadedData["history"].Count != 0)
             {
-                Vector3 pos = boardRef.transform.position + new Vector3(0, -30 * i);
-                GameObject boardTab = Instantiate(boardRef, pos, boardRef.transform.rotation, parent.transform);
-                boardTab.name = "board" + (i + 1);
-                boardTab.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = " " + loadedData["history"][i][0];
-                boardTab.transform.GetChild(7).GetComponent<TextMeshProUGUI>().text = " " + loadedData["history"][i][1];
-                boardTab.transform.GetChild(8).GetComponent<TextMeshProUGUI>().text = " " + loadedData["history"][i][2];
-            }
-            if (loadedData["history"].Count < 16) // Vérification qui permet la disparition de la barre de scroll si le tableau n'est pas assez grand
-            {
-                GameObject.Find("Scroll View").GetComponent<ScrollRect>().enabled = false;
-                GameObject.Find("Scrollbar Vertical").SetActive(false);
+                for (int i = 0; i < loadedData["history"].Count; i++)
+                {
+                    GameObject boardTab = GameObject.Find("board"+(i+1));
+                    boardTab.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = " " + history[i, 0];
+                    boardTab.transform.GetChild(7).GetComponent<TextMeshProUGUI>().text = " " + history[i, 1];
+                    boardTab.transform.GetChild(8).GetComponent<TextMeshProUGUI>().text = " " + history[i, 2];
+                }
             }
         }
-        Destroy(boardRef);
     }
 
     // Méthode de tri du tableau des scores
@@ -61,7 +92,6 @@ public class LeaderboardManager : MonoBehaviour
         if (loadedData["history"].Count != 0)
         {
             //Debug.Log("Tri par " + type);
-            string[,] history = new string[loadedData["history"].Count, 3];
             for (int i = 0; i < loadedData["history"].Count; i++)
             {
                 history[i, 0] = loadedData["history"][i][0];
@@ -74,11 +104,13 @@ public class LeaderboardManager : MonoBehaviour
                 case "difficulty":
                     g = GameObject.Find("diff").transform.GetChild(0).transform;
                     GameObject.Find("time").transform.GetChild(0).transform.rotation = new Quaternion(0, 0, 0, 0);
+                    comparer = new Array2DSort(history, 1);
                     comparer = new Array2DSort(history, 0);
                     break;
                 case "timer":
                     g = GameObject.Find("time").transform.GetChild(0).transform;
                     GameObject.Find("diff").transform.GetChild(0).transform.rotation = new Quaternion(0, 0, 0, 0);
+                    comparer = new Array2DSort(history, 1);
                     comparer = new Array2DSort(history, 2);
                     break;
                 default:
@@ -86,18 +118,14 @@ public class LeaderboardManager : MonoBehaviour
                     break;
             }
             g.rotation = Quaternion.Euler(0, 0, 180);
-            string[,] sortedData = comparer.ToSortedArray();
-            string res = "[\n";
+            string[,] sortedData = comparer.ToSortedArray(); //Tri
             for (int i = 0; i < loadedData["history"].Count; i++)
             {
-                res += "\t[";
-                res += sortedData[i, 0] + ", ";
-                res += sortedData[i, 1] + ", ";
-                if(i != loadedData["history"].Count-1) res += sortedData[i, 2] + "],\n";
-                else res += sortedData[i, 2] + "]\n";
+                history[i, 0] = sortedData[i, 0];
+                history[i, 1] = sortedData[i, 1];
+                history[i, 2] = sortedData[i, 2];
             }
-            res += "]";
-            Debug.Log(res);
+            updateLeaderboard();
         }
     }
 
@@ -139,9 +167,19 @@ public class LeaderboardManager : MonoBehaviour
             return CompareStrings(x, y, _sortIndex);
         }
 
+        // Méthode qui permet de mettre les différentes règles de tri
         protected int CompareStrings(int x, int y, int col)
         {
-            return _sortArray[x, col].CompareTo(_sortArray[y, col]);
+            int res = 0;
+            res = _sortArray[x, col].CompareTo(_sortArray[y, col]);
+            if (col == 0 && _sortArray[x, col] == _sortArray[y, col])
+            {
+                if(_sortArray[x, col + 1] == _sortArray[y, col + 1]) res = _sortArray[x, col + 2].CompareTo(_sortArray[y, col + 2]);
+                else res = _sortArray[x, col + 1].CompareTo(_sortArray[y, col + 1]);
+            }
+            if (_sortArray[x, col] == "Medium" && _sortArray[y, col] == "Hard") res = -1;
+            if (_sortArray[x, col] == "Hard" && _sortArray[y, col] == "Medium") res = 1;
+            return res;
         }
     }
 }

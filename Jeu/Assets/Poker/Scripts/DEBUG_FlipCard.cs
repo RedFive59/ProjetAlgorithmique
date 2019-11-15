2a5337c1077a0228814bc857d6b23b323a37003e
Antoine <minighost@free.fr>
2019-11-15 15:30:59 +0100

    Ajout de la fonction "FlipCard" (pas d'utilisation)
    Création de la fonction permettant de retourner une carte ainsi que d'une fonction DEBUG

---
+++ Jeu/Assets/Poker/Scripts/DEBUG_FlipCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUG_FlipCard : MonoBehaviour
+{
+    FlipCard flipper;
+    Carte modele;
+
+    public GameObject carte;
+
+    void Awake()
+    {
+        modele = carte.GetComponent<Carte>();
+        flipper = carte.GetComponent<FlipCard>();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,10,100,28), "Flip it!"))
+        {
+            flipper.flipCard(modele.cardFace, modele.cardFace);
+        }
+    }
+}

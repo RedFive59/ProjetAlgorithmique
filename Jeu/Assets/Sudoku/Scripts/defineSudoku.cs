bc37e261283d58e404b0741ffa64a7819093340f
Charles <ctacquet@outlook.fr>
2019-11-17 22:06:09 +0100

    Fix problème Sudoku

--- Jeu/Assets/Sudoku/Scripts/defineSudoku.cs
+++ Jeu/Assets/Sudoku/Scripts/defineSudoku.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 
 public static class defineSudoku
 {
-    #if UNITY_EDITOR
     public static string cheminSauvegarde = Path.Combine(Application.dataPath, "StreamingAssets/SudokuLevels/sauvegardeSudoku.json");
-    #elif UNITY_IOS
-    public static string cheminSauvegarde = Path.Combine(Application.dataPath, "StreamingAssets/SudokuLevels/sauvegardeSudoku.json");
-    #elif UNITY_ANDROID
-    public static string cheminSauvegarde = Path.Combine("jar:file://" + Application.dataPath, "StreamingAssets/SudokuLevels/sauvegardeSudoku.json");
-    #endif
     public static string cheminLeaderboard = Path.Combine(Application.dataPath, "StreamingAssets/Leaderboard/leaderboardSudoku.json");
 
     public static string getCheminDifficulte(string difficulte)
